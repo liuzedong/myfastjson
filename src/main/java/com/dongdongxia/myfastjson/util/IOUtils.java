b74063150228602e253e1e28a3603915db526d8b
java_liudong@163.com <dong520>
2017-04-26 18:45:08 +0800

    IOUtils中添加静态初始化块,加载myfastjson.properties配置文件

--- src/main/java/com/dongdongxia/myfastjson/util/IOUtils.java
+++ src/main/java/com/dongdongxia/myfastjson/util/IOUtils.java
@@ -1,5 +1,9 @@
 package com.dongdongxia.myfastjson.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.Properties;
 
 /**
@@ -9,7 +13,14 @@ import java.util.Properties;
  * @author java_liudong@163.com  2017年4月26日 下午4:30:29
  */
 public class IOUtils {
-	
+
+	/**
+	 * 默认的配置文件
+	 */
+	public final static String MYFASTJSON_PROPERTIES = "myfastjson.properties";
+	/**
+	 * 默认加载的myfastjson.properties 配置文件,使用此默认Properties 对象进行加载
+	 */
 	public final static Properties DEFAULT_PROPERTIES = new Properties();
 
 	// 用来,在指定的字符上, 标识,这个数字为真, A-Z(65-90), a-z(97-122), _(95), 0-9
@@ -31,6 +42,52 @@ public class IOUtils {
 		}
 	}
 	
+	// 初始化是否,加载默认的properties文件
+	static {
+		try {
+			loadPropertiesFromFile();
+		} catch (Throwable e){
+			// skip
+		}
+	}
+	
+	/**
+	 * 
+	 * <p>Title: loadPropertiesFromFile</p>
+	 * <p>Description: 加载myfastjson.properties配置文件</p>
+	 * @author java_liudong@163.com  2017年4月26日 下午6:44:23
+	 */
+	public static void loadPropertiesFromFile(){
+		InputStream inputStream = AccessController.doPrivileged(new PrivilegedAction<InputStream>() {
+			@Override
+			public InputStream run() {
+				ClassLoader cl = Thread.currentThread().getContextClassLoader();
+				if (cl != null){
+					return cl.getResourceAsStream(MYFASTJSON_PROPERTIES);
+				} else {
+					return ClassLoader.getSystemResourceAsStream(MYFASTJSON_PROPERTIES);
+				}
+			}
+		});
+		
+		if (null != inputStream){
+			try {
+				DEFAULT_PROPERTIES.load(inputStream);
+				inputStream.close();
+			} catch (IOException e) {
+				// skip
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * <p>Title: getStringProperty</p>
+	 * <p>Description: 先获取系统的环境变量, 没有找到就去myfastjson.properties中查找</p>
+	 * @param name key
+	 * @return  value
+	 * @author java_liudong@163.com  2017年4月26日 下午6:42:10
+	 */
 	public static String getStringProperty(String name){
 		String prop = null;
 		try {
