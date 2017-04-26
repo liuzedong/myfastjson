948e1f0385d8e825956fdc0eb893ff88cbd74f13
java_liudong@163.com <dong520>
2017-04-26 16:43:38 +0800

    JSON类中,添加默认 解析方式和序列化方式, 每个字段使用一个mask 位表示,| 则改位上为1

--- src/main/java/com/dongdongxia/myfastjson/JSON.java
+++ src/main/java/com/dongdongxia/myfastjson/JSON.java
@@ -1,5 +1,9 @@
 package com.dongdongxia.myfastjson;
 
+import com.dongdongxia.myfastjson.parser.Feature;
+import com.dongdongxia.myfastjson.serializer.SerializerFeature;
+import com.dongdongxia.myfastjson.util.IOUtils;
+
 
 /**
  * 
@@ -9,6 +13,46 @@ package com.dongdongxia.myfastjson;
  */
 public abstract class JSON implements JSONStreamAware, JSONAware{
 
+	// 初始化, 默认解析特性
+	public static int DEFAULT_PARSER_FEATURE;
+	// 这里使用 | 来添加解析的默认特性, 因为mask 是每一个字段的一个位标, 比如 1000 | 0100 | 0010 = 1110
+	static {
+		int features = 0;
+		features |= Feature.AutoCloseSource.getMask();
+		features |= Feature.InternFieldNames.getMask();
+		features |= Feature.UseBigDecimal.getMask();
+		features |= Feature.AllowUnQuotedFieldNames.getMask();
+		features |= Feature.AllowSingleQuotes.getMask();
+		features |= Feature.AllowArbitrayCommas.getMask();
+		features |= Feature.SortFeidFastMatch.getMask();
+		features |= Feature.IgnoreNotMatch.getMask();
+		DEFAULT_PARSER_FEATURE = features;
+	}
+	
+	public static int DEFAULT_GENERATE_FEATURE;
+	static {
+		int features = 0;
+		features |= SerializerFeature.QuoteFieldNames.getMask();
+		features |= SerializerFeature.SkipTransientField.getMask();
+		features |= SerializerFeature.WriteEnumUsingName.getMask();
+		features |= SerializerFeature.SortField.getMask();
+		
+		{
+			// 检测本地是否有myfastjsonerializerFeatures.MapSortField 这个字段
+			String featuresProperty = IOUtils.getStringProperty("myfastjsonerializerFeatures.MapSortField");
+			int mask = SerializerFeature.MapSortField.getMask();
+			if ("true".equals(featuresProperty)){
+				features |= mask;
+			} else if ("false".equals(featuresProperty)){
+				features &= ~mask;
+			}
+		}
+		DEFAULT_GENERATE_FEATURE = features;
+	}
+	
+	public static String getName(){
+		return "刘东";
+	}
 	
 	@Override
 	public String toString() {
