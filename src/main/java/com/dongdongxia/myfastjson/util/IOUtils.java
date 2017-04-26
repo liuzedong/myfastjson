f65981e61ae6b716caa521362deb07595b231a87
java_liudong@163.com <dong520>
2017-04-26 18:12:38 +0800

    IOUtils工具类中添加,静态构造方法,初始化时, 把ACSSI码表中的A-Z,a-z,_,0-9的位置,在boolean数组中的位置制为true

--- src/main/java/com/dongdongxia/myfastjson/util/IOUtils.java
+++ src/main/java/com/dongdongxia/myfastjson/util/IOUtils.java
@@ -12,6 +12,25 @@ public class IOUtils {
 	
 	public final static Properties DEFAULT_PROPERTIES = new Properties();
 
+	// 用来,在指定的字符上, 标识,这个数字为真, A-Z(65-90), a-z(97-122), _(95), 0-9
+	public final static boolean identifierFlags[] = new boolean[256];
+	
+	
+	static {
+		// 此处的c=0, 是指,从ASCII码表的 第一个字符开始
+		for (char c = 0; c < identifierFlags.length; ++c){
+			if (c >= 'A' && c <= 'Z'){
+				identifierFlags[c] = true;
+			} else if (c >= 'a' && c <= 'z'){
+				identifierFlags[c] = true;
+			} else if (c == '_'){
+				identifierFlags[c] = true;
+			} else if (c >= '0' && c <= '9'){
+				identifierFlags[c] = true;
+			}
+		}
+	}
+	
 	public static String getStringProperty(String name){
 		String prop = null;
 		try {
