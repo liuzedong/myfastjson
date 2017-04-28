f2abc157fa4b46ba72932dc2f5961ca368300929
java_liudong@163.com <dong520>
2017-04-28 14:11:01 +0800

    添加序列化过滤器的接口,用于标识子类的作用

---
+++ src/main/java/com/dongdongxia/myfastjson/serializer/SerializeFilter.java
@@ -0,0 +1,10 @@
+package com.dongdongxia.myfastjson.serializer;
+/**
+ * 
+ * <P>Description: 定义一个空接口, 用来标识, 子类是序列化的过滤器</P>
+ * @ClassName: SerializeFilter
+ * @author java_liudong@163.com  2017年4月28日 下午2:09:32
+ */
+public interface SerializeFilter {
+
+}
