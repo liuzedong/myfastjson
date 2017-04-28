33189afa6c676ab3269e306793a9581cd0e91e6e
java_liudong@163.com <dong520>
2017-04-28 14:14:55 +0800

    添加,JSON 序列化过滤的控制器,控制器中管理着多个JSON过滤器

---
+++ src/main/java/com/dongdongxia/myfastjson/serializer/JSONSerializer.java
@@ -0,0 +1,10 @@
+package com.dongdongxia.myfastjson.serializer;
+/**
+ * 
+ * <P>Description: JSON序列化控制器</P>
+ * @ClassName: JSONSerializer
+ * @author java_liudong@163.com  2017年4月28日 下午2:14:19
+ */
+public class JSONSerializer extends SerializerFilterable{
+
+}
---
+++ src/main/java/com/dongdongxia/myfastjson/serializer/SerializerFilterable.java
@@ -0,0 +1,12 @@
+package com.dongdongxia.myfastjson.serializer;
+/**
+ * 
+ * <P>Description: 序列化可选过滤器, 包含多个过滤器, 在ObjectSerializer 中使用到的该类的子类</P>
+ * @ClassName: SerializerFilterable
+ * @author java_liudong@163.com  2017年4月28日 下午2:05:34
+ */
+public abstract class SerializerFilterable {
+
+	
+	
+}
