b553d3a697b6eac86a1313ca31e414f787543547
liudong <java_liudong@163.com>
2017-05-10 09:45:40 +0800

    对SerializeWriterTest.java 重命名, 名称打错啦

---
+++ src/test/java/com/dongdongxia/myfastjson/SerializeWriterTest.java
@@ -0,0 +1,25 @@
+package com.dongdongxia.myfastjson;
+/**
+ * 
+ * <P>Description: 序列化写入测试类</P>
+ * @ClassName: SerilizeWriterTest
+ * @author java_liudong@163.com  2017年5月10日 上午8:41:54
+ */
+public class SerializeWriterTest {
+
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * 
+	 * <p>Title: testWriteChar</p>
+	 * <p>Description: writer(int ) 方法的使用</p>
+	 * @author java_liudong@163.com  2017年5月10日 上午9:43:22
+	 */
+	public static void testWriteChar(){
+		
+	}
+	
+}
