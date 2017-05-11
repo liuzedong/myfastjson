7d906bcc28753a8d61ae739b68072902e4e437d7
liudong <java_liudong@163.com>
2017-05-11 19:13:32 +0800

    添加, 编码UTF8格式,和解码UTF8格式

--- src/main/java/com/dongdongxia/myfastjson/util/IOUtils.java
+++ src/main/java/com/dongdongxia/myfastjson/util/IOUtils.java
@@ -2,6 +2,7 @@ package com.dongdongxia.myfastjson.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Properties;
@@ -98,4 +99,55 @@ public class IOUtils {
 		return (prop == null) ? DEFAULT_PROPERTIES.getProperty(name) : prop;
 	}
 	
+	/**
+	 * 
+	 * <p>Title: encodeUTF8</p>
+	 * <p>Description: 将char[] 数组中的字符,以UTF8的编码格式转换为 byte[] 数组中, 如果目标源中有数据, 会清空滴</p>
+	 * <p>FastJson 中, ASCCI直接转换, 中文由两个字节,转换为三个字节, 根据UTF8原理进行转换</p>
+	 * <p>本人能力有限, 根据JDK自带的转换吧</p>
+	 * @param sa char[] 转换源
+	 * @param sp 转换源的起始位置
+	 * @param len 需要转换的长度
+	 * @param da 转换到的byte[] 数组
+	 * @return 返回 目标源中的最后元素的位置
+	 * @author java_liudong@163.com  2017年5月11日 下午5:22:38
+	 */
+	public static int encodeUTF8(char[] sa, int sp, int len, byte[] da) {
+		int dp = 0;
+		byte[] da2 = String.valueOf(sa).substring(sp, sp + len).getBytes(Charset.forName("UTF-8"));
+		dp = da2.length;
+		
+		System.arraycopy(da2, 0, da, 0, dp);
+		
+		return dp;
+	}
+	
+	/**
+	 * 
+	 * <p>Title: decodeUTF8</p>
+	 * <p>Description: 将字节数组以UTF8的形式,转换为char字符, 和上面一样的, char里面的有值的话,那么就直接删除啦</p>
+	 * <p>FastJson 中, ASCCI直接转换, 中文由两个字节,转换为三个字节, 根据UTF8原理进行转换, 源码中定义的, 没有定义的字符编码, 就会返回-1</p>
+	 * <p>本人能力有限, 根据JDK自带的转换吧</p>
+	 * @param sa 源数据
+	 * @param sp 源数据起始位置
+	 * @param len 转换源数据长度
+	 * @param da 目标容器
+	 * @return 目标最后元素的数据
+	 * @author java_liudong@163.com  2017年5月11日 下午5:44:16
+	 */
+	public static int decodeUTF8(byte[] sa, int sp, int len, char[] da) {
+		int dp = 0;
+		
+		final int sl = sp + len;
+		// 先截取
+		byte[] desSa = new byte[len];
+		while(sp < sl) {
+			desSa[dp++] = sa[sp++];
+		}
+		
+		char[] strDa = new String(desSa, Charset.forName("UTF-8")).toCharArray();
+		System.arraycopy(strDa, 0, da, 0, dp);
+		
+		return dp;
+	}
 }
