b04dbeefa6db471a8f0ae93623b39c8758aad382
liudong <java_liudong@163.com>
2017-05-19 10:12:12 +0800

    添加, 向缓存中添加以单引号包括的字符串

--- src/main/java/com/dongdongxia/myfastjson/serializer/SerializeWriter.java
+++ src/main/java/com/dongdongxia/myfastjson/serializer/SerializeWriter.java
@@ -1437,6 +1437,96 @@ public final class SerializeWriter extends Writer{
 		buf[newcount - 1] = ':';
 	}
 	
+	
+	/**
+	 * 
+	 * <p>Title: writeStringWithSingleQuote</p>
+	 * <p>Description: 向缓存中,添加引号 包装的 字符串</p>
+	 * @param text 字符串
+	 * @author java_liudong@163.com  2017年5月19日 上午9:49:30
+	 */
+	protected void writeStringWithSingleQuote(String text) {
+		// 如果为null的情况
+		if (text == null) {
+			int newcount = count + 4;
+			if (newcount > buf.length) {
+				expandCapacity(newcount);
+			}
+			"null".getChars(0, 4, buf, count);
+			count = newcount;
+			return ;
+		}
+		
+		// 如果有值的情况
+		int len = text.length();
+		int newcount = count + len + 2;
+		if (newcount > buf.length) {
+			if (writer != null) {
+				write('\'');
+				for (int i = 0; i < text.length(); ++i) {
+					char ch = text.charAt(i);
+					if (ch <= 13 || ch == '\\' || ch == '\'' || ch == '/' && isEnable(SerializerFeature.WriteSlashAsSpecial)) {
+						write('\\');
+						write(IOUtils.replaceChars[(int) ch]);
+					} else {
+						write(ch);
+					}
+				}
+				write('\'');
+				return ;
+			}
+			expandCapacity(newcount);
+		}
+		
+		int start = count + 1;
+		int end = start + len;
+		
+		buf[count] = '\'';
+		text.getChars(0, len, buf, start);
+		count = newcount;
+		
+		int specialCount = 0;
+		int lastSpecialIndex = -1;
+		char lastSpecial = '\0';
+		for (int i = start; i < end; ++i) {
+			char ch = buf[i];
+			if (ch <= 13 || ch == '\\' || ch == '\'' || ch == '/' && isEnable(SerializerFeature.WriteSlashAsSpecial)) {
+				specialCount++;
+				lastSpecialIndex = i;
+				lastSpecial = ch;
+			}
+		}
+		
+		newcount += specialCount;
+		if (newcount > buf.length) {
+			expandCapacity(newcount);
+		}
+		count = newcount;
+		
+		if (specialCount == 1) {
+			System.arraycopy(buf, lastSpecialIndex + 1, buf, lastSpecialIndex + 2, end - lastSpecialIndex - 1);
+			buf[lastSpecialIndex] = '\\';
+			buf[++lastSpecialIndex] = IOUtils.replaceChars[(int) lastSpecial];
+		} else if (specialCount > 1) {
+			System.arraycopy(buf, lastSpecialIndex + 1, buf, lastSpecialIndex + 2, end - lastSpecialIndex - 1);
+			buf[lastSpecialIndex] = '\\';
+			buf[++lastSpecialIndex] = IOUtils.replaceChars[(int) lastSpecial];
+			end++;
+			for (int i = lastSpecialIndex - 2; i >= start; --i) {
+				char ch = buf[i];
+				
+				if (ch <= 13 || ch == '\\' || ch == '\'' || ch == '/' && isEnable(SerializerFeature.WriteSlashAsSpecial)) {
+					System.arraycopy(buf, i + 1, buf, i + 2, end - i - 1);
+					buf[i] = '\\';
+					buf[i + 1] = IOUtils.replaceChars[(int) ch];
+					end++;
+				}
+			}
+		}
+		buf[count - 1] = '\'';
+	}
+	
+	
 	/**
 	 * 
 	 * <p>Title: append</p>
