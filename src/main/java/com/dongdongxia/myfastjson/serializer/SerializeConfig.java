71bb93d7a5319a674f5463bf1b50adf0756916b7
liudong <java_liudong@163.com>
2017-05-25 11:45:44 +0800

    添加: 序列化配置对象

---
+++ src/main/java/com/dongdongxia/myfastjson/serializer/SerializeConfig.java
@@ -0,0 +1,10 @@
+package com.dongdongxia.myfastjson.serializer;
+/**
+ * 
+ * <P>Description: 序列化配置对象</P>
+ * @ClassName: SerializeConfig
+ * @author java_liudong@163.com  2017年5月25日 上午11:45:06
+ */
+public class SerializeConfig {
+
+}
