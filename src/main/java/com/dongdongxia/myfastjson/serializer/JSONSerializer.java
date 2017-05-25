e0a5a9e767e22df3ab507a13d50db636a79ca70d
liudong <java_liudong@163.com>
2017-05-25 11:48:43 +0800

    添加: 两个全局变量, 构造方法进行初始化

--- src/main/java/com/dongdongxia/myfastjson/serializer/JSONSerializer.java
+++ src/main/java/com/dongdongxia/myfastjson/serializer/JSONSerializer.java
@@ -7,6 +7,14 @@ package com.dongdongxia.myfastjson.serializer;
  */
 public class JSONSerializer extends SerializerFilterable{
 
+	/** 两个主要对象, 用来控制配置和输出, 在构造方法中初始化*/
+	protected final SerializeConfig config;
+	public final SerializeWriter out;
+	
+	public JSONSerializer(SerializeWriter out, SerializeConfig config) {
+		this.out = out;
+		this.config = config;
+	}
 	
 	protected final void writeKeyValue(char seperator, String key, Object value){
 		
