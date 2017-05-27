f2dcf5f1e1829556cb59cd583ffba9ad2de47233
liudong <java_liudong@163.com>
2017-05-27 14:54:48 +0800

    添加: 类描述

--- src/main/java/com/dongdongxia/myfastjson/serializer/SerialContext.java
+++ src/main/java/com/dongdongxia/myfastjson/serializer/SerialContext.java
@@ -1,14 +1,14 @@
 package com.dongdongxia.myfastjson.serializer;
 /**
  * 
- * <P>Description: 串行上下文, 用来装父类,父类的, 一级一级的向上累加</P>
+ * <P>Description: 串行上下文, 用来装父类,父类的, 一级一级的向上累加, 属于一个链表结构</P>
  * @ClassName: SerialContext
  * @author java_liudong@163.com  2017年5月26日 下午4:29:31
  */
 public class SerialContext {
 
-	public final SerialContext parent;
-	public final Object object;
+	public final SerialContext parent; // 父对象
+	public final Object object; // 当前对象
 	public final Object fieldName;
 	public final int features;
 	
