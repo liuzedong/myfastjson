7a8f12f3ad87d82e756a14259deef6084893042a
liudong <java_liudong@163.com>
2017-05-27 10:09:15 +0800

    添加: 初始化方法, 和单例 获取全局实例

--- src/main/java/com/dongdongxia/myfastjson/serializer/SerializeConfig.java
+++ src/main/java/com/dongdongxia/myfastjson/serializer/SerializeConfig.java
@@ -7,4 +7,42 @@ package com.dongdongxia.myfastjson.serializer;
  */
 public class SerializeConfig {
 
+	// 使用单例获取全局的配置
+	public static final SerializeConfig globalInstance = new SerializeConfig();
+	
+	
+	public SerializeConfig() {
+		this(1024);
+	}
+	
+	/**
+	 * 
+	 * <p>Title: 初始化容器,并确定是否使用ASM</p>
+	 * <p>Description: Method for constructor</p>
+	 * @param fieldBase
+	 */
+	public SerializeConfig(boolean fieldBase) {
+		this(1024, fieldBase);
+	}
+	
+	/**
+	 * 
+	 * <p>Title: Constructor</p>
+	 * <p>Description: 初始化容器大小</p>
+	 * @param tableSize
+	 */
+	public SerializeConfig(int tableSize) {
+		this(tableSize, false);
+	}
+	
+	/**
+	 * 
+	 * <p>Title: Constructor</p>
+	 * <p>Description: 将序列化和反序列化 实现进行初始化, 放入到容器</p>
+	 * @param tableSize 容器大小
+	 * @param fieldBase 基础字段
+	 */
+	public SerializeConfig(int tableSize, boolean fieldBase) {
+		
+	}
 }
