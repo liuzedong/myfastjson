ac50d63d60141f122dc9cc55e5888c7f3c40f30b
liudong <java_liudong@163.com>
2017-06-01 15:47:52 +0800

    添加: 获取输出配置对象的方法

--- src/main/java/com/dongdongxia/myfastjson/serializer/JSONSerializer.java
+++ src/main/java/com/dongdongxia/myfastjson/serializer/JSONSerializer.java
@@ -390,6 +390,17 @@ public class JSONSerializer extends SerializerFilterable{
 		this.out.writeNull();
 	}
 	
+	/**
+	 * 
+	 * <p>Title: getMapping</p>
+	 * <p>Description: 获取输出配置对象</p>
+	 * @return
+	 * @author java_liudong@163.com  2017年6月1日 下午3:47:25
+	 */
+	public SerializeConfig getMapping() {
+		return config;
+	}
+	
 	protected final void writeKeyValue(char seperator, String key, Object value){
 		
 	}
