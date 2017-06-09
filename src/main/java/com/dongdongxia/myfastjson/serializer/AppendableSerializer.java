b4bd2a176b50504e7e29647194143e7ba5a4256d
liudong <java_liudong@163.com>
2017-06-09 14:27:29 +0800

    添加: 将流中输出, 当前StringBuffer or StringBuilder or Appendable 中的数据

---
+++ src/main/java/com/dongdongxia/myfastjson/serializer/AppendableSerializer.java
@@ -0,0 +1,26 @@
+package com.dongdongxia.myfastjson.serializer;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+/**
+ * 
+ * <P>Description: StringBuffer, Appendable, StringBuilder 等连接的序列化对象</P>
+ * @ClassName: AppendableSerializer
+ * @author java_liudong@163.com  2017年6月9日 下午2:24:11
+ */
+public class AppendableSerializer implements ObjectSerializer{
+
+	@Override
+	public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType, int features) throws IOException {
+		if (object == null) { // 为null  就输出""
+			SerializeWriter out = serializer.out;
+			out.writeNull(SerializerFeature.WriteNullStringAsEmpty);
+			return ;
+		}
+		
+		serializer.write(object.toString());
+		
+	}
+
+}
