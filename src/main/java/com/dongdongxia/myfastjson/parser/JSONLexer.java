ef94cd3f26907aa4c5f15a9117fc4c649ef10f6a
liudong <java_liudong@163.com>
2017-06-28 09:16:32 +0800

    添加: 解析接口

---
+++ src/main/java/com/dongdongxia/myfastjson/parser/JSONLexer.java
@@ -0,0 +1,10 @@
+package com.dongdongxia.myfastjson.parser;
+/**
+ * 
+ * <P>Description: JSON词法分析器接口</P>
+ * @ClassName: JSONLexer
+ * @author java_liudong@163.com  2017年4月26日 下午4:50:54
+ */
+public interface JSONLexer {
+
+}
