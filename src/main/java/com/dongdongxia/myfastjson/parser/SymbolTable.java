bff3cd12ebcdf28fa4afed4e22ea748c334ddd3e
liudong <java_liudong@163.com>
2017-06-29 15:44:52 +0800

    添加: 封装符号表的对象

---
+++ src/main/java/com/dongdongxia/myfastjson/parser/SymbolTable.java
@@ -0,0 +1,10 @@
+package com.dongdongxia.myfastjson.parser;
+/**
+ * 
+ * <P>Description: 符号表</P>
+ * @ClassName: SymbolTable
+ * @author java_liudong@163.com  2017年6月29日 下午3:44:22
+ */
+public class SymbolTable {
+
+}
